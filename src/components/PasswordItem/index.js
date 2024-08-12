b2e26e009f4b0bf28bfa25edbde55cf49b90c29c
Varun Vaggu <varunvaggu@gmail.com>
2024-08-12 18:12:04 +0530

    first commit

---
+++ src/components/PasswordItem/index.js
@@ -0,0 +1,45 @@
+import './index.css'
+
+const PasswordItem = props => {
+  const {record, showPassword, deletePasswordRecord} = props
+  const {id, url, name, password, profilePicColor} = record
+
+  const deleteItem = () => {
+    deletePasswordRecord(id)
+  }
+
+  const passwordPattern = showPassword ? (
+    <p className="website-text">{password}</p>
+  ) : (
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/password-manager-stars-img.png"
+      alt="stars"
+      className="stars-icon"
+    />
+  )
+
+  return (
+    <li className="password-item">
+      <div className={`circle ${profilePicColor}`}>{name.charAt(0)}</div>
+      <div className="details-container">
+        <p className="website-text">{url}</p>
+        <p className="website-text">{name}</p>
+        {passwordPattern}
+      </div>
+      <button
+        type="button"
+        className="delete-btn"
+        onClick={deleteItem}
+        data-testid="delete"
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
+          alt="delete"
+          className="delete-icon"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default PasswordItem
